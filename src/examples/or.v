0e9f8c83f8b9090195212b9b01b682ab2b409e54
Talia Ringer <talia@dependenttyp.es>
2022-04-27 13:14:17 -0500

    Tej example proof

--- src/examples/or.v
+++ src/examples/or.v
@@ -1,7 +1,31 @@
-From iris_simp_lang Require Import simp.
-From iris Require Import options.
+From iris.heap_lang Require Import proofmode notation.
 
-Definition or: val :=  λ: "x" "y",
-  if: "x" then #true else "y".
+Set Default Proof Using "Type".
 
-(* TODO relate with orb and prove somehow (confused) *)
+(*
+ * This example comes from Tej Chajed.
+ * It is a version of or that short-circuits, so that
+ * it is possible to write a simple proof.
+ *)
+Definition or: val :=
+  λ: "x" "y", if: "x" then #true else "y".
+
+Section heap.
+  Context `{!heapGS Σ}.
+
+  Lemma wp_short_circuit_or (x y: bool) :
+    {{{ True }}}
+      or #x #y
+    {{{ (r:bool), RET #r; ⌜r = orb x y⌝ }}}.
+  Proof.
+    iIntros (Φ) "Hpre HΦ".
+    rewrite /or.
+    wp_pures.
+    destruct x; wp_pures.
+    - iIntros "!>".
+      iApply "HΦ". done.
+    - iIntros "!>".
+      iApply "HΦ". done.
+  Qed.
+
+End heap.
